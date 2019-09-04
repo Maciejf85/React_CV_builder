2af595a090171f57391bd408b180f6529302f131
Maciej Fiałkowski <fialek85@gmail.com>
2019-09-04 20:30:40 +0200

    login files + data

--- src/views/Edit.js
+++ src/views/Edit.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import NavBar from 'components/organisms/Navigation/NavBar';
+import axios from 'axios';
+import path from '../path';
 
 const Edit = () => {
+  axios
+    .post(`${path.cors}login.php`, {
+      text: 'fialek85@gmail.com',
+      password: '123',
+    })
+    .then(response => console.log('response', response))
+    .catch(error => {
+      console.log('error :', error);
+    });
+
   return (
     <>
       <NavBar />
