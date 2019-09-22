7c97ddd1772b0c20d94a283cdd910323c25bdd1a
Maciej Fiałkowski <fialek85@gmail.com>
2019-09-22 20:31:30 +0200

    handle userData

--- src/components/organisms/EditMainContent/UserData.js
+++ src/components/organisms/EditMainContent/UserData.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 const StyledWrapper = styled.div`
   width: 100%;
-  background: #000;
+  background: #333;
   color: white;
 `;
 
-export default class UserData extends Component {
+class UserData extends Component {
   state = {
     name: 'UserData',
   };
 
   render() {
-    const { name } = this.state;
-    return <StyledWrapper>{name}</StyledWrapper>;
+    // const { name } = this.state;
+    const { id, name, surname } = this.props;
+    return (
+      <StyledWrapper>
+        <div>{name}</div>
+        <div>{id}</div>
+        <div>{surname}</div>
+      </StyledWrapper>
+    );
   }
 }
+
+const mapStateToProps = state => state.userData;
+console.log('mapStateToProps', mapStateToProps);
+export default connect(mapStateToProps)(UserData);
