74077f995c3492356c415edf0f59d7703d17b251
Maciej Fiałkowski <fialek85@gmail.com>
2019-10-28 16:25:27 +0100

    languages

--- src/components/organisms/EditMainContent/Languages.js
+++ src/components/organisms/EditMainContent/Languages.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import EducationPanel from 'components/molecules/SectionInputs/EducationPanel';
+import store from 'store';
+import { connect } from 'react-redux';
+import { addNewItemToCurrentCv } from 'actions';
+import { setNewCurrentCVData } from 'functions';
 
-const StyledWrapper = styled.div`
-  width: 100%;
-  background: #000;
-  color: white;
-`;
-
-export default class Languages extends Component {
-  state = {
-    name: 'Languages',
-  };
+class Languages extends Component {
+  componentDidUpdate() {
+    const { cvId, currentCv } = this.props;
+    const token = sessionStorage.getItem('userID');
+    store.dispatch(setNewCurrentCVData('update', token, cvId, currentCv));
+  }
 
   render() {
-    const { name } = this.state;
-    return <StyledWrapper>{name}</StyledWrapper>;
+    const { cvId, currentCv } = this.props;
+    const { languages } = currentCv;
+    return (
+      <>
+        {languages &&
+          languages.map((item, idx) => {
+            const { id } = item;
+            return <EducationPanel key={id} index={idx} item={item} cvId={cvId} />;
+          })}
+        <button
+          type="button"
+          onClick={() =>
+            store.dispatch(
+              addNewItemToCurrentCv('languages', {
+                name: '',
+                startYear: 2000,
+                startMonth: 1,
+                endYear: 2000,
+                endMonth: 1,
+                description: '',
+              }),
+            )
+          }
+        >
+          Dodaj nową szkołę
+        </button>
+      </>
+    );
   }
 }
+
+const mapStateToProps = state => ({
+  currentCv: state.currentCv,
+  cvId: state.currentCv.currentItem.id,
+});
+
+export default connect(mapStateToProps)(Languages);
