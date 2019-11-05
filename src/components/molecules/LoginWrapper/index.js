186b272a7d22766ac392cce5561362cbbedf53cb
Maciej Fiałkowski <Fialek85@gmail.com>
2019-11-05 18:04:21 +0100

    login componennt

---
+++ src/components/molecules/LoginWrapper/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const LoginWrapper = styled.div`
+width:270px;
+padding:20px;
+border:1px solid black;
+border-radius:7px;
+`
+
+export default LoginWrapper
--- src/views/Login.js
+++ src/views/Login.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Footer from 'components/organisms/Footer/Footer';
 import store from 'store';
+import image from 'assets/login.png';
+import LoginWrapper from 'components/molecules/LoginWrapper'
 import { getMainData } from 'actions';
 
 const StyledWrapper = styled.div`
   /* margin: 0 20px; */
-  /* border: 1px solid orange; */
 `;
+const MainWrapper = styled.div`
+margin-top:100px;
+display:flex;
+justify-content:center;
+align-items:center;
+width:100%;
+min-height:360px;
+img{
+  opacity:0.8;
+  margin:20px;
+}
+`
 
 class Login extends Component {
   componentDidMount() {
@@ -18,6 +31,16 @@ class Login extends Component {
     return (
       <>
         <StyledWrapper>
+          <MainWrapper>
+            <div>Zaloguj</div>
+            <button type='button' onClick={() => this.props.history.push('/main')} > go to main</button>
+            <img src={image} alt='user' width='80px' />
+            <LoginWrapper>
+
+            </LoginWrapper>
+            {/* <SignIn/>
+            <SignUp/> */}
+          </MainWrapper>
           <Footer />
         </StyledWrapper>
       </>
