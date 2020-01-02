8dfa149ad866c1cf4a27dc445eeedf581e7d71cc
Maciej Fiałkowski <fialek85@gmail.com>
2020-01-02 12:17:55 +0100

    template component

--- src/components/molecules/loginComponents/signUp.js
+++ src/components/molecules/loginComponents/signUp.js
@@ -10,10 +10,14 @@ import { connect } from 'react-redux';
 
 class SignUp extends Component {
   state = {
-    login: `${Math.random().toFixed(2) * 10000000}@wp.pl`,
-    password: '123456',
-    name: 'Maciej',
-    surname: 'Fiałkowski',
+    login: ``,
+    // login: `${Math.random().toFixed(2) * 10000000}@wp.pl`,
+    password: '',
+    // password: '123456',
+    name: '',
+    // name: 'Maciej',
+    surname: '',
+    // surname: 'Fiałkowski',
     nameValid: '',
     surnameValid: '',
     loginValid: '',
