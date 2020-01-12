865598830f1c06ab250cedf732d6f488ff58125d
Maciej Fiałkowski <fialek85@gmail.com>
2020-01-12 12:40:08 +0100

    secondStyle left side + masking

--- src/components/themes/SecondStyle.js
+++ src/components/themes/SecondStyle.js
@@ -22,24 +22,30 @@ Font.register({
 
 const MainContainer = styled.Page`
   position: relative;
-  display: inline-flex;
+  display: flex;
   flex-direction: row;
   background-color: white;
   padding-bottom: 100pt;
+  padding-top: 15pt;
 `;
 const Masking = styled.View`
   position: absolute;
-  width: 430pt;
-  height: 16pt;
+  left: 0;
   top: 0;
-  left: 165pt;
-  background: white;
+  width: 595pt;
+  height: 16pt;
+`;
+const MaskContent = styled.View`
+  width: 176pt;
+  height: 100%;
+  margin-left: 28pt;
+  background: #eee5e6;
 `;
 
 const LeftColumn = styled.View`
   width: 176pt;
-  height: 705pt;
-  color: red;
+  overflow: hidden;
+  color: #444444;
   padding: 0 17pt;
   margin-left: 28pt;
   margin-top: 36pt;
@@ -48,7 +54,6 @@ const LeftColumn = styled.View`
 `;
 const RightColumn = styled.View`
   width: 391pt;
-  height: 100%;
   margin-top: 144pt;
   font-family: 'Montserrat';
   padding: 0 19pt;
@@ -123,6 +128,80 @@ const HeadContainerLeft = styled.View`
   height: 140pt;
 `;
 
+const Icon = styled.Image`
+  width: 15pt;
+  height: 9pt;
+  background-position: center center;
+`;
+
+const PersonalDataSection = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SectionLeft = styled.Text`
+  margin: 0 0 10pt;
+  color: #444444;
+  font-size: 9pt;
+  padding: 5pt 0 0 11pt;
+`;
+
+const ContentTitleBox = styled.View`
+  margin: 25pt 0;
+  font-size: 11pt;
+  text-transform: uppercase;
+`;
+const ContentTitle = styled.Text`
+  font-size: 11pt;
+  text-transform: uppercase;
+  letter-spacing: 0.5pt;
+  font-weight: semiBold;
+`;
+const TitleDecoration = styled.View`
+  width: 30pt;
+  height: 5pt;
+  border-bottom: 3pt solid ${({ dark }) => (dark ? '#444444' : 'white')};
+`;
+
+const SectionTitleLeft = styled.Text`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  text-transform: capitalize;
+  color: #444444;
+  font-size: 9.8pt;
+  font-weight: semiBold;
+  letter-spacing: 0.2pt;
+`;
+const Link = styled.Link`
+  margin: 0 0 10pt;
+  color: #444444;
+  font-size: 8.5pt;
+  padding: 5pt 0 0 11pt;
+  font-family: 'Montserrat';
+  font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
+`;
+
+const TextSection = styled.View`
+  position: relative;
+  /* margin-bottom: 10pt; */
+  padding-right: 10pt;
+`;
+
+const SectionLeftBox = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionTitleLeftDot = styled.View`
+  margin: 0 4pt;
+  width: 4pt;
+  height: 4pt;
+  background: white;
+  border-radius: 50pt;
+`;
+
 const Footer = styled.View`
   position: absolute;
   display: flex;
@@ -138,7 +217,7 @@ const Footer = styled.View`
 const FooterText = styled.Text`
   align-items: flex-end;
   font-family: 'Montserrat';
-  color: black;
+  color: #444444;
   font-size: 8pt;
   line-height: 1.5pt;
   padding: 5px 0;
@@ -169,6 +248,9 @@ class MyDocument extends Component {
     return (
       <Document title={currentItem.title} author="Maciej Fiałkowski">
         <MainContainer size="A4">
+          <Masking fixed>
+            <MaskContent />
+          </Masking>
           <TopHeader>
             <UserNameContainer bold>
               <NameAndSurname>
@@ -183,6 +265,107 @@ class MyDocument extends Component {
             <HeadContainerLeft>
               {!state.image.image || <ImageRound src={state.image.image} />}
             </HeadContainerLeft>
+
+            <ContentTitleBox wrap={false}>
+              <ContentTitle>{language === 'PL' ? 'dane osobowe' : 'personal'}</ContentTitle>
+              <TitleDecoration dark />
+            </ContentTitleBox>
+            {!phone || (
+              <PersonalDataSection>
+                <SectionTitleLeft white bold>
+                  <Icon src={phoneIcon} /> {language === 'PL' ? 'telefon' : 'phone'}:
+                </SectionTitleLeft>
+                <SectionLeft>{phone}</SectionLeft>
+              </PersonalDataSection>
+            )}
+            {!email || (
+              <PersonalDataSection>
+                <SectionTitleLeft white bold>
+                  <Icon src={emailIcon} /> Email:
+                </SectionTitleLeft>
+                <SectionLeft>{email}</SectionLeft>
+              </PersonalDataSection>
+            )}
+            {!adress || (
+              <PersonalDataSection>
+                <SectionTitleLeft white bold>
+                  <Icon src={addressIcon} /> {language === 'PL' ? 'adres' : 'address'}:
+                </SectionTitleLeft>
+                <SectionLeft>{adress}</SectionLeft>
+              </PersonalDataSection>
+            )}
+            {!github || (
+              <>
+                <SectionTitleLeft white bold>
+                  <Icon src={githubIcon} /> Github:
+                </SectionTitleLeft>
+                <PersonalDataSection>
+                  <Link href={github} target="_blank" rel="noopener noreferrer">
+                    {github}
+                  </Link>
+                </PersonalDataSection>
+              </>
+            )}
+            {!linkedin || (
+              <>
+                <SectionTitleLeft white bold>
+                  <Icon src={linkedIcon} /> Linkedin:
+                </SectionTitleLeft>
+                <PersonalDataSection>
+                  <Link href={linkedin} target="_blank" rel="noopener noreferrer">
+                    {linkedin}
+                  </Link>
+                </PersonalDataSection>
+              </>
+            )}
+
+            {!languages.length || (
+              <ContentTitleBox wrap={false}>
+                <ContentTitle>{language === 'PL' ? 'Języki obce' : 'Languages'}</ContentTitle>
+                <TitleDecoration white />
+              </ContentTitleBox>
+            )}
+
+            {languages.map(item => (
+              <TextSection key={item.id} wrap={false}>
+                <SectionLeftBox>
+                  <SectionTitleLeftDot />
+                  <SectionTitleLeft bold>{item.name}</SectionTitleLeft>
+                </SectionLeftBox>
+                <SectionLeft>{`${item.description}`}</SectionLeft>
+              </TextSection>
+            ))}
+
+            {!skills.length || (
+              <ContentTitleBox wrap={false}>
+                <ContentTitle>{language === 'PL' ? 'Umiejętności' : 'Skills'}</ContentTitle>
+                <TitleDecoration white />
+              </ContentTitleBox>
+            )}
+
+            {skills.map(item => (
+              <TextSection key={item.id} wrap={false}>
+                <SectionLeftBox>
+                  <SectionTitleLeftDot />
+                  <SectionTitleLeft bold>{item.name}</SectionTitleLeft>
+                </SectionLeftBox>
+                <SectionLeft>{`${item.description || ''}`}</SectionLeft>
+              </TextSection>
+            ))}
+            {!interests.length || (
+              <ContentTitleBox wrap={false}>
+                <ContentTitle>{language === 'PL' ? 'zainteresowania' : 'interests'}</ContentTitle>
+                <TitleDecoration />
+              </ContentTitleBox>
+            )}
+            {interests.map(item => (
+              <TextSection key={item.id} wrap={false}>
+                <SectionLeftBox>
+                  <SectionTitleLeft bold>{item.name}</SectionTitleLeft>
+                </SectionLeftBox>
+                <SectionLeft>{`${item.description || ''}`}</SectionLeft>
+              </TextSection>
+            ))}
           </LeftColumn>
           <RightColumn />
 
